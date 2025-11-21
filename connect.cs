d73cee0a2dd2772a0b028f79996de2057c9a2578
chombatee <128930015+chombatee@users.noreply.github.com>
2025-11-21 21:59:16 +0200

    Modify connection string for SQL Server Express
    Updated connection string to use SQL Server Express with Windows Authentication.

--- connect.cs
+++ connect.cs
@@ -1,13 +1,13 @@
-﻿namespace part_1
+namespace part_1
 {
     public class connect
     {
 
         public string connecting()
         {
-
-            //then return connection
-            return "Data Source=(localdb)\\claim_system;Initial Catalog=claiming_system;";
+            // Updated connection string to use SQL Server Express with Windows Authentication
+            // Make sure the database exists on the server
+            return "Server=LabVM1846780\\SQLEXPRESS01;Database=claiming_system;Trusted_Connection=True;TrustServerCertificate=True;";
         }
     }
 }
